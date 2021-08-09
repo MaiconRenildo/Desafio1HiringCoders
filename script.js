696cfa6c2ada604d1b4ffb3b67e450a618748b94
Maicon Renildo <maicon.renildo1@gmail.com>
2021-08-08 23:33:16 -0300

    Alteracao do footer

---
+++ script.js
@@ -0,0 +1,36 @@
+class Form{
+  constructor(name,email){
+    this.name=name;
+    this.email=email
+  }
+}
+
+function ResetForm(){
+  document.getElementById('name').value=''
+  document.getElementById('e-mail').value=''
+}
+function Envio(evt){
+  
+  
+  let storage=localStorage.getItem('dadosCadastro')
+  let name=document.getElementById('name').value
+  let email=document.getElementById('e-mail').value
+
+  if(name.trim()=='' || email.trim()==''){
+    console.log('vazio')
+  }else{
+    let dados=new Form(name,email)
+    if(storage==null){
+      let objeto=[]
+      objeto.push(dados)
+      localStorage.setItem('dadosCadastro',JSON.stringify(objeto))
+    }else{
+      let array=JSON.parse(storage)
+      array.push(dados)
+      localStorage.setItem('dadosCadastro',JSON.stringify(array))
+    }
+
+    ResetForm()
+    evt.preventDefault()
+  }
+}
